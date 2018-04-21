5f381076b5f5ad7a6f30cbeb031df6b5a396ba1f
colin4k <colin4k@gmail.com>
2018-04-21 09:00:19 +0800

    init

---
+++ src/main/java/com/gzcb/webfluxdemo/router/CityRouter.java
@@ -0,0 +1,16 @@
+package com.gzcb.webfluxdemo.router;
+
+import com.gzcb.webfluxdemo.handler.CityHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+public class CityRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> routeCity(CityHandler cityHandler){
+        return RouterFunctions.route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),cityHandler::helloCity);
+    }
+}
